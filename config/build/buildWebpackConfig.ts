8ac098ca82c22a7597c3083054ccf0047735d32c
Alexander <alexander.tishininov@gmail.com>
2023-12-02 13:38:52 +0500

    build: add and configure webpack-dev-server

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,5 +1,3 @@
-import path from "path";
-
 import webpack from "webpack";
 
 import { BuildOptions } from "./types/config";
@@ -7,11 +5,13 @@ import { BuildOptions } from "./types/config";
 import { buildPlugins } from "./buildPlugins";
 import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
     const {
         paths,
-        mode
+        mode,
+        isDev
     } = options;
     
     return {
@@ -27,5 +27,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders()
         },
         resolve: buildResolvers(),
+        devtool: isDev ? "inline-source-map" : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined
     };
 }
