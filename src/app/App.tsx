0ff46e6eee028017ad23214dffc9835d4bf77d1c
Alexander <alexander.tishininov@gmail.com>
2023-12-04 07:29:52 +0500

    feat(navbar): add navbar with links

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,15 +4,15 @@ import { Suspense } from "react";
 import { useTheme } from "app/providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
 
 const App = () => {
     const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={classNames("app", {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={"/"}>Главная</Link>
-            <Link to={"/about"}>О сайте</Link>
             <AppRouter />
         </div>
     );
